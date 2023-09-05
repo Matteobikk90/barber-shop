4f5c0408492058a686a5c8743c1839e61fe805ea
Matteo Soresini <matteo.soresini@hotmail.it>
2023-09-05 11:02:13 +0200

    console log removed

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
     res.json({
-        message: "Welcome to Bledon booking API." + process.env.MYSQL_PASSWORD
+        message: "Welcome to Bledon booking API."
     });
 });
 
