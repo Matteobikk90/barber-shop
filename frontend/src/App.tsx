11c2114c4943efb446a964e9cbb5b0915dca71c4
Matteo Soresini <matteo.soresini@hotmail.it>
2023-09-19 12:11:20 +0200

    SEO react helmet async

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,12 @@ import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import { routers } from "routes";
 
-const App = () => {
-    return (
-        <BarberShopionContextProvider>
-            <Suspense fallback={<>...</>}>
-                <RouterProvider router={routers} />
-            </Suspense>
-        </BarberShopionContextProvider>
-    );
-};
+const App = () => (
+    <BarberShopionContextProvider>
+        <Suspense fallback={<>...</>}>
+            <RouterProvider router={routers} />
+        </Suspense>
+    </BarberShopionContextProvider>
+);
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "reportWebVitals";
 import { GlobalStyles } from "twin.macro";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -11,8 +12,40 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <StrictMode>
-        <App />
-        <GlobalStyles />
+        <HelmetProvider>
+            <Helmet prioritizeSeoTags>
+                <title>Blendon Barber Shop</title>
+                <meta
+                    name="description"
+                    content="Blendon Barber Shop è un barbiere di Rivoli, (To) gestito da Luca Frasca"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    href="https://blendon-barber-shop.it/favicon.svg"
+                />
+                <link
+                    rel="manifest"
+                    href="https://blendon-barber-shop.it/manifest.json"
+                />
+                <meta name="robots" content="index, follow" />
+                <meta
+                    property="og:url"
+                    content="https://blendon-barber-shop.it"
+                />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Blendon Barber Shop" />
+                <meta
+                    property="og:description"
+                    content="Blendon Barber Shop è un barbiere di Rivoli, (To) gestito da Luca Frasca"
+                />
+                <meta
+                    property="og:image"
+                    content="https://blendon-barber-shop.it/favicon.svg"
+                />
+            </Helmet>
+            <App />
+            <GlobalStyles />
+        </HelmetProvider>
     </StrictMode>
 );
 
