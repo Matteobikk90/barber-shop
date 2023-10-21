00c855aa52a49c2005ae6a1439b7411a4c3a1073
Matteo Soresini <matteo.soresini@hotmail.it>
2023-10-21 20:45:21 +0200

    nodemailer sending email, folder renamed and unnecessary files removed

---
+++ client/src/services/addBooking.ts
@@ -0,0 +1,66 @@
+import { FormEvent } from "react";
+import { BookingTypes } from "types/booking.types";
+import { db } from "db/firebase";
+import { collection, setDoc, Timestamp, doc } from "firebase/firestore";
+
+export const handleSubmitBooking = async (
+    e: FormEvent,
+    {
+        service,
+        name,
+        surname,
+        start_time,
+        end_time,
+        phone,
+        email,
+        readable_start_time
+    }: BookingTypes,
+    next: () => void
+) => {
+    e.preventDefault();
+    try {
+        const myCollection = collection(db, "bookings");
+        const newBooking = {
+            service,
+            name,
+            surname,
+            start_time,
+            end_time,
+            phone,
+            email,
+            id: Timestamp.now(),
+            readable_start_time
+        };
+        const emailBody = {
+            name,
+            surname,
+            email,
+            phone,
+            readable_start_time,
+            service
+        };
+        const myDocRef = doc(
+            myCollection,
+            `${surname} ${readable_start_time
+                .split(" ")[0]
+                .replaceAll("/", "-")} ${readable_start_time
+                .split(" ")[1]
+                .replaceAll("/", "-")}`
+        );
+        await setDoc(myDocRef, newBooking);
+        await fetch("http://localhost:8080/send_email", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            mode: "cors",
+            body: JSON.stringify(emailBody)
+        })
+            .then(() => next())
+            .catch(() =>
+                alert(
+                    "Email non inviata, ma la prenotazione è comunque andata a buon fine. A presto"
+                )
+            );
+    } catch (_) {
+        alert("Qualcosa è andato storto, per favore riprova");
+    }
+};
