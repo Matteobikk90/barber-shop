89f8adba9f5933fe6aeec0250923a2ca94a09b0f
Matteo Soresini <matteo.soresini@hotmail.it>
2024-09-09 16:50:38 +0200

    new url

--- client/src/hooks/useContactForm.ts
+++ client/src/hooks/useContactForm.ts
@@ -2,58 +2,56 @@ import { FormEvent, useState } from "react";
 import { FormDataTypes, InputTypes } from "types/contact-form.types";
 
 const useContactForm = (
-    initialState: FormDataTypes = {
-        name: "",
-        phone: null,
-        email: "",
-        message: "",
-        confirmation: false
-    }
+  initialState: FormDataTypes = {
+    name: "",
+    phone: null,
+    email: "",
+    message: "",
+    confirmation: false,
+  }
 ) => {
-    const [formData, setFormData] = useState(initialState);
+  const [formData, setFormData] = useState(initialState);
 
-    const handleInputChange = ({ name, value, type, checked }: InputTypes) =>
-        setFormData({
-            ...formData,
-            [name]: type === "checkbox" ? checked : value
-        });
+  const handleInputChange = ({ name, value, type, checked }: InputTypes) =>
+    setFormData({
+      ...formData,
+      [name]: type === "checkbox" ? checked : value,
+    });
 
-    const handleReset = () =>
-        setFormData({
-            name: "",
-            phone: null,
-            email: "",
-            message: "",
-            confirmation: false
-        });
+  const handleReset = () =>
+    setFormData({
+      name: "",
+      phone: null,
+      email: "",
+      message: "",
+      confirmation: false,
+    });
 
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const emailBody = {
-            name: formData.name,
-            email: formData.email,
-            phone: formData.phone,
-            message: formData.message
-        };
-        await fetch(
-            "https://barber-shop-backend-efc4.onrender.com/send_contact_email",
-            {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                mode: "cors",
-                body: JSON.stringify(emailBody)
-            }
-        )
-            .then(() => {
-                alert(
-                    "Grazie per averci contattato. Ti risponderemo il prima possibile"
-                );
-                handleReset();
-            })
-            .catch(() => alert("Messaggio non inviato, riprovare più tardi"));
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const emailBody = {
+      name: formData.name,
+      email: formData.email,
+      phone: formData.phone,
+      message: formData.message,
     };
+    // https://barber-shop-backend-efc4.onrender.com
+    await fetch(`${process.env.REACT_APP_NEW_URL_EMAIL}send_contact_email`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+      body: JSON.stringify(emailBody),
+    })
+      .then(() => {
+        alert(
+          "Grazie per averci contattato. Ti risponderemo il prima possibile"
+        );
+        handleReset();
+      })
+      .catch(() => alert("Messaggio non inviato, riprovare più tardi"));
+  };
 
-    return { formData, handleInputChange, handleSubmit };
+  return { formData, handleInputChange, handleSubmit };
 };
 
 export default useContactForm;
--- client/src/services/addBooking.ts
+++ client/src/services/addBooking.ts
@@ -4,67 +4,65 @@ import { collection, setDoc, Timestamp, doc } from "firebase/firestore";
 import { Dispatch, SetStateAction } from "react";
 
 export const handleSubmitBooking = async (
-    {
-        service,
-        name,
-        surname,
-        start_time,
-        end_time,
-        phone,
-        email,
-        readable_start_time
-    }: BookingTypes,
-    next: () => void,
-    setIsLoading: Dispatch<SetStateAction<boolean>>
+  {
+    service,
+    name,
+    surname,
+    start_time,
+    end_time,
+    phone,
+    email,
+    readable_start_time,
+  }: BookingTypes,
+  next: () => void,
+  setIsLoading: Dispatch<SetStateAction<boolean>>
 ) => {
-    try {
-        const myCollection = collection(db, "bookings");
-        const newBooking = {
-            service,
-            name,
-            surname,
-            start_time,
-            end_time,
-            phone,
-            email,
-            id: Timestamp.now(),
-            readable_start_time
-        };
-        const emailBody = {
-            name,
-            surname,
-            email,
-            phone,
-            readable_start_time,
-            service
-        };
-        const myDocRef = doc(
-            myCollection,
-            `${surname.replaceAll(" ", "")} ${readable_start_time
-                .split(" ")[0]
-                .replaceAll("/", "-")} ${readable_start_time
-                .split(" ")[1]
-                .replaceAll("/", "-")}`
-        );
-        await setDoc(myDocRef, newBooking);
-        await fetch(
-            "https://barber-shop-backend-efc4.onrender.com/send_email_booking",
-            {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                mode: "cors",
-                body: JSON.stringify(emailBody)
-            }
+  try {
+    const myCollection = collection(db, "bookings");
+    const newBooking = {
+      service,
+      name,
+      surname,
+      start_time,
+      end_time,
+      phone,
+      email,
+      id: Timestamp.now(),
+      readable_start_time,
+    };
+    const emailBody = {
+      name,
+      surname,
+      email,
+      phone,
+      readable_start_time,
+      service,
+    };
+    const myDocRef = doc(
+      myCollection,
+      `${surname.replaceAll(" ", "")} ${readable_start_time
+        .split(" ")[0]
+        .replaceAll("/", "-")} ${readable_start_time
+        .split(" ")[1]
+        .replaceAll("/", "-")}`
+    );
+    // https://barber-shop-backend-efc4.onrender.com
+    await setDoc(myDocRef, newBooking);
+    await fetch(`${process.env.REACT_APP_NEW_URL_EMAIL}send_email_booking`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      mode: "no-cors",
+      body: JSON.stringify(emailBody),
+    })
+      .then(() => next())
+      .catch(() =>
+        alert(
+          "Email non inviata, ma la prenotazione è comunque andata a buon fine. A presto"
         )
-            .then(() => next())
-            .catch(() =>
-                alert(
-                    "Email non inviata, ma la prenotazione è comunque andata a buon fine. A presto"
-                )
-            );
-    } catch (_) {
-        alert("Qualcosa è andato storto, per favore riprova");
-    } finally {
-        setIsLoading(false);
-    }
+      );
+  } catch (_) {
+    alert("Qualcosa è andato storto, per favore riprova");
+  } finally {
+    setIsLoading(false);
+  }
 };
