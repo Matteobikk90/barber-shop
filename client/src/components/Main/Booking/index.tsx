caccc1687791528fd15428ee67bba7101f9472ab
Matteo Soresini <matteo.soresini@hotmail.it>
2024-10-24 11:11:17 +0400

    date blocked

--- client/src/components/Main/Booking/index.tsx
+++ client/src/components/Main/Booking/index.tsx
@@ -6,107 +6,102 @@ import Success from "components/Main/Booking/Success";
 import { handleSubmitBooking } from "services/addBooking";
 import { BookingTypes } from "types/booking.types";
 import useMultiStepForm from "hooks/useMultiStepForm";
-import Loader from "components/Loader";
 /** @jsxImportSource @emotion/react */
 import tw from "twin.macro";
 
 const INITIAL_DATA: BookingTypes = {
-    service: "",
-    name: "",
-    surname: "",
-    email: "",
-    phone: null,
-    start_time: "",
-    readable_start_time: "",
-    end_time: "",
-    confirmation: false,
-    handleBookingInfo: () => {},
-    handleInputChange: () => {}
+  service: "",
+  name: "",
+  surname: "",
+  email: "",
+  phone: null,
+  start_time: "",
+  readable_start_time: "",
+  end_time: "",
+  confirmation: false,
+  handleBookingInfo: () => {},
+  handleInputChange: () => {},
 };
 
 const Booking = ({ handleToggleState }: any) => {
-    const [booking, setBooking] = useState(INITIAL_DATA);
-    const bookingFormRef = useRef<HTMLFormElement | null>(null);
-    const [isLoading, setIsLoading] = useState(false);
+  const [booking, setBooking] = useState(INITIAL_DATA);
+  const bookingFormRef = useRef<HTMLFormElement | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleBookingInfo = (bookinfInfo: Partial<BookingTypes>) => {
-        setBooking((prev) => ({ ...prev, ...bookinfInfo }));
-        !isSecondLastStep && next();
-    };
+  const handleBookingInfo = (bookinfInfo: Partial<BookingTypes>) => {
+    setBooking((prev) => ({ ...prev, ...bookinfInfo }));
+    !isSecondLastStep && next();
+  };
 
-    const handleInputChange = ({ checked }: any) =>
-        setBooking({
-            ...booking,
-            confirmation: checked
-        });
+  const handleInputChange = ({ checked }: any) =>
+    setBooking({
+      ...booking,
+      confirmation: checked,
+    });
 
-    const submitBooking = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setIsLoading(true);
-        handleSubmitBooking(booking, next, setIsLoading);
-    };
+  const submitBooking = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    handleSubmitBooking(booking, next, setIsLoading);
+  };
 
-    const {
-        back,
-        currentStepIndex,
-        next,
-        isFirstStep,
-        isSecondLastStep,
-        isLastStep,
-        step,
-        steps
-    } = useMultiStepForm([
-        <Step1 handleBookingInfo={handleBookingInfo} />,
-        <Step2 {...booking} handleBookingInfo={handleBookingInfo} />,
-        <Step3
-            {...booking}
-            handleInputChange={handleInputChange}
-            handleBookingInfo={handleBookingInfo}
-        />,
-        <Success {...booking} />
-    ]);
+  const {
+    back,
+    currentStepIndex,
+    next,
+    isFirstStep,
+    isSecondLastStep,
+    isLastStep,
+    step,
+    steps,
+  } = useMultiStepForm([
+    <Step1 handleBookingInfo={handleBookingInfo} />,
+    <Step2 {...booking} handleBookingInfo={handleBookingInfo} />,
+    <Step3
+      {...booking}
+      handleInputChange={handleInputChange}
+      handleBookingInfo={handleBookingInfo}
+    />,
+    <Success {...booking} />,
+  ]);
 
-    return (
-        <form
-            ref={bookingFormRef}
-            onSubmit={submitBooking}
-            css={[
-                tw`relative text-center bg-green text-cream shadow min-h-[450px] p-[2rem 5rem 4rem] md:p-[2rem 1rem 4rem] sm:p-[4rem 1rem]`,
-                currentStepIndex === 1 && tw`p-[2rem 5rem 5rem]`
-            ]}
-        >
-            <button
-                tw="absolute top-[1rem] left-[1rem] text-xxl"
-                onClick={() => handleToggleState("isBooking")}
-                type="button"
-            >
-                Chiudi X
-            </button>
-            <div tw="absolute top-[1rem] right-[1rem] text-xxl">
-                {currentStepIndex + 1} / {steps.length}
-            </div>
-            {step}
-            <div tw="text-xxl flex gap-[1rem] absolute bottom-[1rem] right-[1rem] mt-[1rem]">
-                {!isFirstStep && !isLastStep && !isLoading && (
-                    <button type="button" onClick={back}>
-                        Indietro
-                    </button>
-                )}
-                {isSecondLastStep &&
-                booking.name &&
-                booking.surname &&
-                booking.phone &&
-                booking.email &&
-                booking.confirmation ? (
-                    <button type="submit" value="Send">
-                        {isLoading
-                            ? "Prenotazione in corso, attendi"
-                            : "Prenota"}
-                    </button>
-                ) : null}
-            </div>
-        </form>
-    );
+  return (
+    <form
+      ref={bookingFormRef}
+      onSubmit={submitBooking}
+      css={[
+        tw`relative text-center bg-green text-cream shadow min-h-[450px] p-[2rem 5rem 4rem] md:p-[2rem 1rem 4rem] sm:p-[4rem 1rem]`,
+        currentStepIndex === 1 && tw`p-[2rem 5rem 5rem]`,
+      ]}>
+      <button
+        tw="absolute top-[1rem] left-[1rem] text-xxl"
+        onClick={() => handleToggleState("isBooking")}
+        type="button">
+        Chiudi X
+      </button>
+      <div tw="absolute top-[1rem] right-[1rem] text-xxl">
+        {currentStepIndex + 1} / {steps.length}
+      </div>
+      {step}
+      <div tw="text-xxl flex gap-[1rem] absolute bottom-[1rem] right-[1rem] mt-[1rem]">
+        {!isFirstStep && !isLastStep && !isLoading && (
+          <button type="button" onClick={back}>
+            Indietro
+          </button>
+        )}
+        {isSecondLastStep &&
+        booking.name &&
+        booking.surname &&
+        booking.phone &&
+        booking.email &&
+        booking.confirmation ? (
+          <button type="submit" value="Send">
+            {isLoading ? "Prenotazione in corso, attendi" : "Prenota"}
+          </button>
+        ) : null}
+      </div>
+    </form>
+  );
 };
 
 export default Booking;
