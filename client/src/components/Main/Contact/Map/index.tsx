d24ea590457c828999e2465f422a3c9baf4005d3
Matteo Soresini <matteo.soresini@hotmail.it>
2024-11-17 20:05:38 +0400

    fixes

--- client/src/components/Main/Contact/Map/index.tsx
+++ client/src/components/Main/Contact/Map/index.tsx
@@ -1,6 +1,6 @@
 import { memo, useEffect, useState } from "react";
 import useLeaflet from "hooks/useLeaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, Marker, Popup } from "react-leaflet";
 import { LeafletEvent } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { centerMap, defaultMapZoom } from "utils/map";
@@ -8,39 +8,34 @@ import { centerMap, defaultMapZoom } from "utils/map";
 import "twin.macro";
 
 const Map = () => {
-    const [mapRef, setMapRef] = useState<LeafletEvent | null>(null);
-    const { handleResize, marker } = useLeaflet(mapRef);
+  const [mapRef, setMapRef] = useState<LeafletEvent | null>(null);
+  const { handleResize, marker } = useLeaflet(mapRef);
 
-    useEffect(() => {
-        window.addEventListener("resize", handleResize);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
 
-        return () => window.removeEventListener("resize", handleResize);
-    }, [handleResize]);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
 
-    return (
-        <MapContainer
-            whenReady={setMapRef as any}
-            tw="bg-white h-full w-full"
-            center={centerMap}
-            zoom={defaultMapZoom}
-            minZoom={5}
-            maxZoom={18}
-            trackResize={false}
-            touchZoom={false}
-            scrollWheelZoom={false}
-            dragging={false}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={centerMap} icon={marker}>
-                <Popup>
-                    Blendon Barber Shop <br /> Ti aspettiamo !
-                </Popup>
-            </Marker>
-        </MapContainer>
-    );
+  return (
+    <MapContainer
+      whenReady={setMapRef as any}
+      tw="bg-white h-full w-full"
+      center={centerMap}
+      zoom={defaultMapZoom}
+      minZoom={5}
+      maxZoom={18}
+      trackResize={false}
+      touchZoom={false}
+      scrollWheelZoom={false}
+      dragging={false}>
+      <Marker position={centerMap} icon={marker}>
+        <Popup>
+          Blendon Barber Shop <br /> Ti aspettiamo !
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default memo(Map);
--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,4 +1,4 @@
-import Homepage from "components/Main/Homepage";
+// import Homepage from "components/Main/Homepage";
 import Product from "components/Main/Product";
 import Menu from "components/Main/Menu";
 import Gallery from "components/Main/Gallery";
@@ -9,29 +9,28 @@ import BookingBtn from "components/BookingBtn";
 import tw from "twin.macro";
 
 const Main = ({ toggleState, handleToggleState }: any) => (
-    <>
-        {!toggleState.isBooking && (
-            <BookingBtn handleToggleState={handleToggleState} />
-        )}
-        <main
-            css={[
-                tw`pt-[8.75rem] md:pt-[4rem]`,
-                toggleState.isBooking && tw`pt-0 md:pt-0`
-            ]}
-        >
-            {toggleState.isBooking ? (
-                <Booking handleToggleState={handleToggleState} />
-            ) : (
-                <>
-                    {/* <Homepage /> */}
-                    <Product />
-                    <Menu />
-                    <Gallery />
-                    <Contact />
-                </>
-            )}
-        </main>
-    </>
+  <>
+    {!toggleState.isBooking && (
+      <BookingBtn handleToggleState={handleToggleState} />
+    )}
+    <main
+      css={[
+        tw`pt-[8.75rem] md:pt-[4rem]`,
+        toggleState.isBooking && tw`pt-0 md:pt-0`,
+      ]}>
+      {toggleState.isBooking ? (
+        <Booking handleToggleState={handleToggleState} />
+      ) : (
+        <>
+          {/* <Homepage /> */}
+          <Product />
+          <Menu />
+          <Gallery />
+          <Contact />
+        </>
+      )}
+    </main>
+  </>
 );
 
 export default Main;
