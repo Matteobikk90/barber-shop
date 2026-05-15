8f1b6e19784ea010d65bb8e69232a6c7ee0a561c
Matteo Soresini <matteo.soresini@hotmail.it>
2026-05-15 11:11:21 +0200

    refactor: rename Calendario2 to Calendario; simplify routes

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,36 +2,24 @@ import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 export const routers = createBrowserRouter([
-    {
-        children: [
-            {
-                path: "/",
-                Component: lazy(() => import("layouts"))
-            },
-            {
-                path: "/calendario",
-                Component: lazy(() => import("components/Admin"))
-            },
-            {
-                path: "/calendario2",
-                Component: lazy(() => import("components/Admin/Calendario2"))
-            },
-            {
-                path: "/blocco-date",
-                Component: lazy(() => import("components/Admin/BlockDate"))
-            },
-            {
-                path: "/terms-and-conditions",
-                Component: lazy(
-                    () => import("components/Main/TermsAndConditions")
-                )
-            },
-            {
-                path: "/privacy-and-policy",
-                Component: lazy(
-                    () => import("components/Main/PrivacyAndPolicy")
-                )
-            }
-        ]
-    }
+  {
+    children: [
+      {
+        path: "/",
+        Component: lazy(() => import("layouts")),
+      },
+      {
+        path: "/calendario",
+        Component: lazy(() => import("components/Admin/Calendario")),
+      },
+      {
+        path: "/terms-and-conditions",
+        Component: lazy(() => import("components/Main/TermsAndConditions")),
+      },
+      {
+        path: "/privacy-and-policy",
+        Component: lazy(() => import("components/Main/PrivacyAndPolicy")),
+      },
+    ],
+  },
 ]);
